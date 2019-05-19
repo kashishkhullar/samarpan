2991a2ed267390e7655fd055036ec955c3d4bfeb
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2019-05-20 00:55:02 +0530

    modified contract

--- ubbi.sol
+++ ubbi.sol
@@ -26,8 +26,35 @@ contract UBBI{
     address private _reserveBankAddress;
     address private _scheduledBankHQAddress;
     address private _commercialBankHQAddress;
-    uint _scheduledBankBalance;
-    uint _commercialBankBalance;
+    uint private _scheduledBankBalance;
+    uint private _commercialBankBalance;
+    uint private _usableTokens;
+    uint private _reservedTokens;
+
+    modifier onlyReserveBank(){
+        require(msg.sender == _reserveBankAddress);
+        _;
+    }
+
+    modifier onlyScheduled(){
+        require(msg.sender == _scheduledBankHQAddress);
+        _;
+    }
+
+    constructor(uint totalSupply, uint usableTokens) public{
+
+        _reserveBankAddress = msg.sender;
+        _totalSupply = totalSupply;
+        _usableTokens = usableTokens;
+        _reservedTokens = totalSupply - usableTokens;
+
+    }
+
+    function registerScheduledBank(address scheduledBankHQAddress) public onlyReserveBank{
+        require(_reserveBankAddress != address(0));
+        _scheduledBankHQAddress = scheduledBankHQAddress;
+    }
+
 
     /**
     
